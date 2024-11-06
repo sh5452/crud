f2e1f9b739c855ffbdfb6bb0e60c610b6dcee13f
sh5452 <105583484+sh5452@users.noreply.github.com>
2024-11-06 09:12:54 +0200

    a

--- app.js
+++ app.js
@@ -40,29 +40,28 @@ app.post('/api/users',(request,response)=>{
    response.json(update_user)
 })
 
-//PUT
-app.put('/api/users/id',(request,response)=>{
-  const user_body=request.body 
-  const user_id=parseInt(request.params.id )
-  const update_user=crud.put(user_body,user_id)
-  response (update_user)
-
+// PUT
+app.put('/api/users/:id', (request, response) => {
+    const user_body = request.body
+    const user_id = parseInt(request.params.id)
+    const updated_or_created_user = crud.put(user_id, user_body)
+    response.json(updated_or_created_user)
 })
-
-//PATCH
-app.patch('/api/users/:id',(request,response)=>{
-    const user_body=request.body
-    const user_id=parseInt(request.params.id)
-    const update_user=crud.patch(user_body,user_id)
-    if (update_user) {
-        response.json(update_user)
-    }else{
-        response.status(404).json(`error: can't find the id ${user_id}`)
+// PATCH
+app.patch('/api/users/:id', (request, response) => {
+    const user_body = request.body
+    const user_id = parseInt(request.params.id)
+    const updated_user = crud.patch(user_id, user_body)
+    if (updated_user) { 
+        response.json(updated_user)
+    }
+    else {
+        response.status(404).json({ "error": `cannot find user with id ${user_id}`})
     }
 })
 
 //DELETE
-app.delete('/api/:id', (request, response)=>{
+app.delete('/api/users/:id', (request, response)=>{
     const user_id=parseInt(request.params.id)
     const update_user=crud.delete_by_id(user_id)
     if(update_user){
